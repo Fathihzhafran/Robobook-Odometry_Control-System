a3bf1d5d90b3323eca7fb40d76d7d26bdcdea7a5
GHunter57 <89604434+Fathihzhafran@users.noreply.github.com>
2026-01-29 23:32:34 +0700

    Update velocity-asymmetric-hybrid.ino

--- archive/velocity-control-bench-final-v3/velocity-asymmetric-hybrid.ino
+++ archive/velocity-control-bench-final-v3/velocity-asymmetric-hybrid.ino
@@ -1,20 +1,7 @@
 /*
  * Copyright 2026 Fathihzafran
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Project: Robobook Odometry & Control System
- * Component: Advanced Velocity Tuning (Phase 2 - Bench Test)
+ * Project: Robobook Odometry - Hybrid Asymmetric Compensation
  */
 
 #include <ESP32Servo.h>
